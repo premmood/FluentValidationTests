132fc5315451604a98a66c2871f3755a49e7311b
Pramod Chand <pramodchand@gmail.com>
2021-06-13 15:59:30 +0545

    initial

---
+++ FluentApplication/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using FluentApplication.Models;
+using FluentApplication.Validator;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Student stud)// public IActionResult Index([CustomizeValidator(Properties = "Name,EmailId")]Student stud)
+        {
+            //var validator = new StudentValidator();
+
+            //if (true)
+            //{
+            //var results = validator.Validate(stud, options => options.IncludeRuleSets("MyRule"));
+            //var results = validator.Validate(stud, options => options.IncludeProperties("Name", "EmailId"));
+            //results.AddToModelState(ModelState, null);
+            //}
+            ////else 
+            //{
+            //    var resultss = validator.Validate(stud);
+            //    //resultss.IsValid
+            //    //resultss.Errors
+            //    resultss.AddToModelState(ModelState, null);
+            //}
+
+            if (!ModelState.IsValid)
+            { 
+            
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ FluentApplication/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentApplication.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 0;
+        public string EmailId { get; set; } = "";
+    }
+}
